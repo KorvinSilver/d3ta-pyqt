39fbb80459f24d2fe8833f1bf06f609811f5f6e1
Korvin Ezüst <dev@korvin.eu>
2018-03-02 02:49:37 +0100

    delete

--- PassMismatch.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-# Form implementation generated from reading ui file 'PassMismatch.ui'
-#
-# Created: Fri Mar  2 02:46:58 2018
-#      by: pyside2-uic  running on PySide2 2.0.0~alpha0
-#
-# WARNING! All changes made in this file will be lost!
-
